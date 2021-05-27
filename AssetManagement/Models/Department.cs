410aa4ff53509135474f1bd58e179cb50579b45e
Habibie <habibie598@gmail.com>
2021-05-27 20:16:00 +0700

    Update Model Department and Employee

---
+++ AssetManagement/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Models
+{
+    [Table("TB_M_Department")]
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        public string DepartmentName { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
