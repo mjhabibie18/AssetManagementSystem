5d5421cf8883f011a6c9c516875f99e5b941da2f
Habibie <habibie598@gmail.com>
2021-05-28 05:45:25 +0700

    Update Repository

---
+++ AssetManagement/Repositories/Data/DepartmentRepositorycs.cs
@@ -0,0 +1,18 @@
+﻿using AssetManagement.Context;
+using AssetManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Repositories.Data
+{
+    public class DepartmentRepository : GeneralRepository<Department, MyContext, int>
+    {
+        private readonly MyContext myContext;
+        public DepartmentRepository(MyContext myContext) : base(myContext)
+        {
+
+        }
+    }
+}
---
+++ AssetManagement/Repositories/Data/ItemRepository.cs
@@ -0,0 +1,18 @@
+﻿using AssetManagement.Context;
+using assetManaAssetManagementgement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Repositories.Data
+{
+    public class ItemRepository : GeneralRepository<Item, MyContext, int>
+    {
+        private readonly MyContext myContext;
+        public ItemRepository(MyContext myContext) : base(myContext)
+        {
+
+        }
+    }
+}
