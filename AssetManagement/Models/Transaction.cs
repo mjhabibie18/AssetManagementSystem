ddd6e9505237455af9f4788487e9a947171fdbb4
Muljadi <muljadiyadi29@gmail.com>
2021-05-27 20:25:42 +0700

    UpdateModels completed

---
+++ AssetManagement/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assetManagement.Models
+{
+    [Table("TB_T_Transaction")]
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Request { get; set; }
+        public DateTime Return { get; set; }
+        public string Status { get; set; }
+        public Employee Employee { get; set; }
+        public ICollection<TransactionItem> TransactionItems { get; set; }
+    }
+}
---
+++ AssetManagement/Models/TransactionItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assetManagement.Models
+{
+    [Table("TB_T_TransactionItem")]
+    public class TransactionItem
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public Transaction Transaction { get; set; }
+        public Item Item { get; set; }
+
+    }
+}
